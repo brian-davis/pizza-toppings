0119e2117ddea1c30995b69bd7e766954aae0033
Brian Davis <gbriandavis@fastmail.com>
2024-11-09 15:08:24 -0700

    update test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -14,17 +14,10 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_select '#user-header > div:nth-child(3) > form[action="/users/sign_out"] > button', "Sign Out"
   end
 
-  test "user sees dashboard appropriate for role, chef user" do
+  test "user sees dashboard" do
     sign_in users(:chef1)
     get home_index_path
     assert_response :success
-    assert_select "h3.dashboard-header", "Chef Dashboard"
-  end
-
-  test "user sees dashboard appropriate for role, owner user" do
-    sign_in users(:owner1)
-    get home_index_path
-    assert_response :success
-    assert_select "h3.dashboard-header", "Owner Dashboard"
+    assert_select "h3.dashboard-header", "Dashboard"
   end
 end
